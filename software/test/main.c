09cd2e644a5510e9ac291515ea6804efc6bf7f05
progranism <fpgaminer@bitcoin-mining.com>
2012-01-30 00:25:18 -0800

    Tested debugging with the new UART module. Works much faster. Also tested loading in new ELFs to memory with GDB; works. GDB had an issue calculating breakpoint addresses from line numbers though.

--- software/test/main.c
+++ software/test/main.c
@@ -8,7 +8,12 @@ int main(int i, char **c)
 
 	while (1)
 	{
-		CSR_GPIO = count++;
+		if (count == 1000000) {
+			CSR_GPIO = (CSR_GPIO&1) ? 0xF0F0F0F0 : 0x0F0F0F0F;
+			count = 0;
+		}
+
+		count++;
 
 		if (CSR_UART_RX != 0 && CSR_UART_TX == 0) {
 			CSR_UART_TX = CSR_UART_RX & 0xFF;
